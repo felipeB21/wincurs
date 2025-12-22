78cbc706adf71678d207fad6da491168082728d1
felipeB21 <bolgarfelipe@gmail.com>
2025-12-21 21:18:57 -0300

    Added shcema to client db for querys

--- db/index.ts
+++ db/index.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import * as schema from "@/db/schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
 });
 
-export const db = drizzle(pool);
+export const db = drizzle(pool, { schema });
